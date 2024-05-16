d561c767c80ef2d3bfc0b8428b53dabbeceebeb2
unknown <sivaharicr7@gmail.com>
2024-05-16 20:11:40 +0530

    comments added

--- app.py
+++ app.py
@@ -1,34 +1,48 @@
+# Importing necessary modules
+
 import streamlit as st
 from transformers import pipeline, AutoImageProcessor, AutoModelForImageClassification
 from PIL import Image
 import google.generativeai as genai
-import api
-# Gemini AI API
+
+# Gemini AI API-Key
 api_key = "AIzaSyBFvdQlmO4yqipuNs-l7wd37xWgZ34dVV8"
 
 
-# Image classification
+# Setting up image classification emvironment and loading the transformer with pipeline and assigned task
 processor = AutoImageProcessor.from_pretrained("hareeshr/medicinal_plants_image_detection")
 model = AutoModelForImageClassification.from_pretrained("hareeshr/medicinal_plants_image_detection")
 pipeline = pipeline(task="image-classification", model=model, image_processor=processor)
 
 def predict(image):
+
+    # Passing image to the transformer via pipeline to predict the result
     predictions = pipeline(image)
+
+    # Returning the top prediction
     return predictions[0]
 
 def main():
+
+    # Setting up page title
     st.title("Medicinal Plant Classification and Information")
 
+    # Creating a form layout
     with st.form("my_form"):
         uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
+        # Ensuring if the image is uploaded successfully or not.
         if uploaded_file is not None:
+
             # Display the uploaded image
             image = Image.open(uploaded_file)
+            
             st.image(image, caption="Uploaded Image", use_column_width=True)
-
+        
+        # Making the predict button with boolean return type
         clicked = st.form_submit_button("Predict")
 
+        # When the user clicks on 'PREDICT' button
         if clicked:
             try:
                 result = predict(image)
@@ -47,9 +61,13 @@ def main():
                 with st.spinner(f"Generating information about {label}..."):
                     response = model.generate_content(prompt)
                     if response:
+                        
+                        # Writing response to the UI
                         st.write(response.text)
                     else:
                         st.error("Failed to generate information. Please try again later.")
+
+            # Error handling                        
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
 
