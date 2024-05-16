82247b6b0a93233233a701312238fa05d28a42a0
unknown <sivaharicr7@gmail.com>
2024-05-16 09:01:15 +0530

    gemnini ai integrated

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
+import requests
 import pandas as pd
 import streamlit as st
 from transformers import pipeline, AutoImageProcessor, AutoModelForImageClassification
 from PIL import Image
+import google.generativeai as genai
+from google.generativeai import generative_models
 
+# Gemini AI API
+api_key = "AIzaSyBFvdQlmO4yqipuNs-l7wd37xWgZ34dVV8"
+
+
+# Image classification
 processor = AutoImageProcessor.from_pretrained("hareeshr/medicinal_plants_image_detection")
 model = AutoModelForImageClassification.from_pretrained("hareeshr/medicinal_plants_image_detection")
 pipeline = pipeline(task="image-classification", model=model, image_processor=processor)
@@ -12,8 +21,8 @@ def predict(image):
     return predictions[0]
 
 def main():
-    st.title("Image Classification")
-    
+    st.title("Medicinal Plant Classification and Information")
+
     with st.form("my_form"):
         uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
@@ -29,6 +38,15 @@ def main():
             label = result['label']
             score = result['score'] * 100
             st.success(f"The predicted image is {label} with {score:.2f}% confidence.")
+            genai.configure(api_key=api_key)
+            model = genai.GenerativeModel(model_name="gemini-pro")
 
+            prompt = "Provide detailed information about the medicinal plant 'Tulasi'. Please include its botanical name, common names, medicinal properties, traditional uses, active compounds, potential health benefits, and any known contraindications or side effects. Additionally, discuss who can benefit from its usage, such as individuals with specific health conditions or symptoms, and who should avoid it, such as pregnant or breastfeeding women, individuals with certain medical conditions, or those taking specific medications. Please provide evidence-based information and cite credible sources where applicable."
+            # content = [prompt]
+            response = model.generate_content(prompt)
+
+            print(response.text)
+
+            st.write(response.text)
 if __name__ == "__main__":
     main()
